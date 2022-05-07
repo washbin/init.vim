bf58e9beaafb5b2218bdebbf0d9698e3967b36c7
washbin <76929116+washbin@users.noreply.github.com>
2022-05-07 14:10:03 +0545

    refactor: just some file move arounds

---
+++ lua/lsp_config.lua
@@ -0,0 +1,96 @@
+local opts = { noremap = true, silent = true }
+local function set_keymap(...)
+  vim.api.nvim_set_keymap("n", ...)
+end
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+set_keymap("<space>e", "<cmd>lua vim.diagnostic.open_float(0, {scope='line',border='single'})<CR>", opts)
+set_keymap("[d", "<cmd>lua vim.diagnostic.goto_prev({float={border='single'}})<CR>", opts)
+set_keymap("]d", "<cmd>lua vim.diagnostic.goto_next({float={border='single'}})<CR>", opts)
+set_keymap("<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, "n", ...)
+  end
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap("<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  buf_set_keymap("gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  buf_set_keymap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  buf_set_keymap("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  buf_set_keymap("gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  buf_set_keymap("K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  buf_set_keymap("<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  buf_set_keymap("<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  buf_set_keymap("<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+
+  -- buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+  -- buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+  -- buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+
+  -- Set some keybinds conditional on server capabilities
+  if client.resolved_capabilities.document_formatting then
+    buf_set_keymap("<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  elseif client.resolved_capabilities.document_range_formatting then
+    buf_set_keymap("<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  end
+
+  if client.resolved_capabilities.document_highlight then
+    vim.api.nvim_exec(
+      [[
+    hi LspReferenceRead cterm=bold ctermbg=red guibg=LightYellow
+    hi LspReferenceText cterm=bold ctermbg=red guibg=LightYellow
+    hi LspReferenceWrite cterm=bold ctermbg=red guibg=LightYellow
+    augroup lsp_document_highlight
+    autocmd! * <buffer>
+    autocmd CursorHold <buffer> lua vim.lsp.buf.document_highlight()
+    autocmd CursorMoved <buffer> lua vim.lsp.buf.clear_references()
+    augroup END
+    ]],
+      false
+    )
+  end
+end
+
+-- Add additional capabilities supported by nvim-cmp
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- Setup LSP
+require("nvim-lsp-installer").on_server_ready(function(server)
+  if server.name == "tsserver" then
+    server:setup({
+      on_attach = function(client, bufnr)
+        client.resolved_capabilities.document_formatting = false
+        client.resolved_capabilities.document_range_formatting = false
+
+        on_attach(client, bufnr)
+      end,
+      capabilities = capabilities,
+    })
+  else
+    server:setup({ on_attach = on_attach, capabilities = capabilities })
+  end
+end)
+
+local null_ls = require("null-ls")
+null_ls.setup({
+  sources = {
+    null_ls.builtins.code_actions.eslint,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.formatting.prettier,
+
+    null_ls.builtins.diagnostics.flake8,
+    null_ls.builtins.diagnostics.pyproject_flake8,
+    null_ls.builtins.formatting.isort,
+    null_ls.builtins.formatting.black,
+  },
+  on_attach = on_attach,
+})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -11,7 +11,6 @@ require("packer").startup({
     use("hrsh7th/nvim-cmp") -- Autocompletion plugin
     use("hrsh7th/cmp-nvim-lsp") -- LSP source for nvim-cmp
     use("jose-elias-alvarez/null-ls.nvim") -- LSP source for null-ls
-    use("jose-elias-alvarez/nvim-lsp-ts-utils") -- LSP source for typescript
     -- snippets
     use("L3MON4D3/LuaSnip") -- Snippets plugin
     use("saadparwaiz1/cmp_luasnip") -- Snippets source for nvim-cmp
@@ -63,13 +62,12 @@ require("packer").startup({
   },
 })
 
+--- Few liner settings ---
 require("onedark").setup({
   style = "darker",
 })
 require("onedark").load()
-
 require("gitsigns").setup()
-
 require("lualine").setup({
   options = { theme = "onedark" },
 })
@@ -80,194 +78,11 @@ require("nvim-treesitter.configs").setup({
   autotag = { enable = true },
   refactor = { highlight_definitions = { enable = true } },
 })
--- each of these are documented in `:help nvim-tree.OPTION_NAME`
-require("nvim-tree").setup({
-  disable_netrw = true,
-  hijack_netrw = true,
-  open_on_setup = false,
-  ignore_ft_on_setup = {},
-  auto_reload_on_write = true,
-  open_on_tab = false,
-  hijack_cursor = true,
-  update_cwd = false,
-  hijack_directories = {
-    enable = true,
-    auto_open = true,
-  },
-  diagnostics = {
-    enable = false,
-    icons = {
-      hint = "",
-      info = "",
-      warning = "",
-      error = "",
-    },
-  },
-  update_focused_file = {
-    enable = false,
-    update_cwd = false,
-    ignore_list = {},
-  },
-  system_open = {
-    cmd = nil,
-    args = {},
-  },
-  filters = {
-    dotfiles = false,
-    custom = {},
-  },
-  git = {
-    enable = true,
-    ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = "right",
-    mappings = {
-      custom_only = false,
-      list = {},
-    },
-    number = false,
-    relativenumber = false,
-    signcolumn = "yes",
-  },
-  trash = {
-    cmd = "trash",
-    require_confirm = true,
-  },
-  actions = {
-    change_dir = {
-      global = false,
-    },
-    open_file = {
-      quit_on_open = false,
-    },
-  },
-})
-
--- Add additional capabilities supported by nvim-cmp
-local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, ...)
-  end
-  local function buf_set_option(...)
-    vim.api.nvim_buf_set_option(bufnr, ...)
-  end
-
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
-
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-
-  local opts = { noremap = true, silent = true }
-
-  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  buf_set_keymap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  -- buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-  -- buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-  -- buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
-  buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
-  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
-  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-  -- Set some keybinds conditional on server capabilities
-  if client.resolved_capabilities.document_formatting then
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  elseif client.resolved_capabilities.document_range_formatting then
-    buf_set_keymap("n", "<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
-  end
-end
-
-require("nvim-lsp-installer").on_server_ready(function(server)
-  if server.name == "tsserver" then
-    server:setup({
-
-      on_attach = function(client, bufnr)
-        client.resolved_capabilities.document_formatting = false
-        client.resolved_capabilities.document_range_formatting = false
-
-        local ts_utils = require("nvim-lsp-ts-utils")
-        ts_utils.setup({})
-        ts_utils.setup_client(client)
-
-        on_attach(client, bufnr)
-      end,
-      capabilities = capabilities,
-    })
-  else
-    server:setup({ on_attach = on_attach, capabilities = capabilities })
-  end
-end)
-
-local null_ls = require("null-ls")
-null_ls.setup({
-	sources = {
-        null_ls.builtins.diagnostics.eslint,
-        null_ls.builtins.code_actions.eslint,
-        null_ls.builtins.formatting.prettier
-	},
-	on_attach = on_attach
-})
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
 
--- luasnip setup
-local luasnip = require("luasnip")
-
--- nvim-cmp setup
-local cmp = require("cmp")
-cmp.setup({
-  snippet = {
-    expand = function(args)
-      require("luasnip").lsp_expand(args.body)
-    end,
-  },
-  mapping = {
-    ["<C-p>"] = cmp.mapping.select_prev_item(),
-    ["<C-n>"] = cmp.mapping.select_next_item(),
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
-    ["<C-Space>"] = cmp.mapping.complete(),
-    ["<C-e>"] = cmp.mapping.close(),
-    ["<CR>"] = cmp.mapping.confirm({
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    }),
-    ["<Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end,
-    ["<S-Tab>"] = function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end,
-  },
-  -- sources = { { name = "nvim_lsp" }, { name = "luasnip" }, { name = "cmp_tabnine" } },
-  sources = { { name = "nvim_lsp" }, { name = "luasnip" } },
-})
+--- Import settings ---
+require("lsp_config")
+require("nvim_cmp_config")
+require("nvim_tree_config")
