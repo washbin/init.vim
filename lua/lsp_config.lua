35cbd7d087999659dec4efbc3f2d7828cc0d2deb
washbin <76929116+washbin@users.noreply.github.com>
2023-01-04 00:09:50 +0545

    chore: changes for supporting disable format in tsserver and html in latest api

--- lua/lsp_config.lua
+++ lua/lsp_config.lua
@@ -1,14 +1,14 @@
 local opts = { noremap = true, silent = true }
 local function set_keymap(...)
-  vim.api.nvim_set_keymap("n", ...)
+  vim.keymap.set("n", ...)
 end
 
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-set_keymap("<space>e", "<cmd>lua vim.diagnostic.open_float(0, {scope='line',border='single'})<CR>", opts)
-set_keymap("[d", "<cmd>lua vim.diagnostic.goto_prev({float={border='rounded'}})<CR>", opts)
-set_keymap("]d", "<cmd>lua vim.diagnostic.goto_next({float={border='rounded'}})<CR>", opts)
-set_keymap("<space>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
+set_keymap("<space>e", function() vim.diagnostic.open_float(0, {scope='line',border='single'}) end, opts)
+set_keymap("[d", function() vim.diagnostic.goto_prev({float={border='rounded'}}) end, opts)
+set_keymap("]d", function() vim.diagnostic.goto_next({float={border='rounded'}}) end, opts)
+set_keymap("<space>q", vim.diagnostic.setloclist, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
@@ -18,32 +18,33 @@ local on_attach = function(client, bufnr)
   vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
   vim.api.nvim_buf_set_option(bufnr, "tagfunc", "v:lua.vim.lsp.tagfunc")
 
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
   local function buf_set_keymap(...)
-    vim.api.nvim_buf_set_keymap(bufnr, "n", ...)
+    vim.keymap.set("n", ...)
   end
 
   -- Mappings.
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap("<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-  buf_set_keymap("gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-  buf_set_keymap("gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-  buf_set_keymap("gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-  buf_set_keymap("gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-  buf_set_keymap("K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-  buf_set_keymap("<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-  buf_set_keymap("<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-  buf_set_keymap("<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-  buf_set_keymap("<space>sm", "<cmd>lua vim.lsp.buf.document_symbol()<CR>", opts)
+  buf_set_keymap("<space>D", vim.lsp.buf.type_definition, opts)
+  buf_set_keymap("gD", vim.lsp.buf.declaration, opts)
+  buf_set_keymap("gd", vim.lsp.buf.definition, opts)
+  buf_set_keymap("gi", vim.lsp.buf.implementation, opts)
+  buf_set_keymap("gr", vim.lsp.buf.references, opts)
+  buf_set_keymap("K", vim.lsp.buf.hover, opts)
+  buf_set_keymap("<C-k>", vim.lsp.buf.signature_help, opts)
+  buf_set_keymap("<space>rn", vim.lsp.buf.rename, opts)
+  buf_set_keymap("<space>ca", vim.lsp.buf.code_action, opts)
+  buf_set_keymap("<space>sm", vim.lsp.buf.document_symbol, opts)
 
-  -- buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
-  -- buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
-  -- buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+  buf_set_keymap("<space>wa", vim.lsp.buf.add_workspace_folder, opts)
+  buf_set_keymap("<space>wr", vim.lsp.buf.remove_workspace_folder, opts)
+  buf_set_keymap("<space>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
 
   -- Set some keybinds conditional on server capabilities
-  if client.server_capabilities.document_formatting then
-    buf_set_keymap("<space>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
-  elseif client.server_capabilities.document_range_formatting then
-    buf_set_keymap("<space>f", "<cmd>lua vim.lsp.buf.range_formatting()<CR>", opts)
+  if client.server_capabilities.documentFormattingProvider then
+    buf_set_keymap("<space>f", function() vim.lsp.buf.format{ async = true } end, opts)
+  elseif client.server_capabilities.documentRangeFormattingProvider then
+    buf_set_keymap("<space>f", function() vim.lsp.buf.range_format{ async = true } end, opts)
   end
 
   if client.server_capabilities.document_highlight then
@@ -75,8 +76,8 @@ require("nvim-lsp-installer").on_server_ready(function(server)
   if formatting_disabled_servers[server.name] then
     server:setup({
       on_attach = function(client, bufnr)
-        client.server_capabilities.document_formatting = false
-        client.server_capabilities.document_range_formatting = false
+        client.server_capabilities.documentFormattingProvider = false
+        client.server_capabilities.documentRangeFormattingProvider = false
         on_attach(client, bufnr)
       end,
       capabilities = capabilities,
